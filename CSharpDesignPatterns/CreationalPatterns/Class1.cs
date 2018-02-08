eef549fdbdc61335b32b74cd0f995f66996a17a1
Prashant Kumar Verma <prashant.kumar.verma@nl.abnamro.com>
2018-02-08 18:10:53 +0100

    added
    added

---
+++ CSharpDesignPatterns/CreationalPatterns/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPatterns
+{
+    public class Class1
+    {
+    }
+}
